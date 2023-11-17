bccb0b770d946d89239e1bb871950515c5a89bdc
李明 <1811481344@qq.com>
2023-11-17 15:01:54 +0800

    主页布局

---
+++ src/components/right/recentPost.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {computed} from "vue";
+import {useWindowScroll} from "@vueuse/core";
+
+const { x, y } = useWindowScroll()
+
+const shouldFixTop = computed(() => {
+  return y.value >= 70
+})
+
+</script>
+
+<template>
+  <div class="recent-post" :class="{ 'fixed-top': shouldFixTop }">
+    recentPost
+  </div>
+</template>
+
+<style scoped>
+.recent-post{
+  width: 300px;
+  height: 450px;
+  background-color: #1a1a1a;
+  margin-top: 20px;
+  margin-left: 30px;
+  border-radius: 15px;
+}
+.fixed-top {
+  position: fixed;
+  top: 0;
+  margin-left: 30px;
+}
+</style>
